d50610b81d171cabec12f037b6def6818889d35c
Laxaro-Void <alvaro.rojasva@usm.cl>
2026-04-11 22:39:46 -0400

    Fix merge pr

--- Anbu/main.go
+++ Anbu/main.go
@@ -8,16 +8,225 @@ import (
 	"os"
 	// "time"
 
-
-	//pb "Akatsuki/proto/message"
-
-	//"github.com/rabbitmq/amqp091-go"
-	//"google.golang.org/grpc"
+	pb "Anbu/proto/DataAkatsuki"
+	"github.com/rabbitmq/amqp091-go"
+	"google.golang.org/protobuf/proto"
 )
+
+type Akatsukis struct {
+	Id int
+	Name string
+	Ataque int
+	Vida int
+	Estado string
+	isComunicated bool
+}
+
+func dialRabbitMQ() (*amqp091.Connection, error) {
+	log.Printf("Connecting to RabbitMQ at %s", "amqp://guest:guest@" + os.Getenv("RABBITMQ-IP") + "/")
+	conn, err := amqp091.Dial("amqp://guest:guest@" + os.Getenv("RABBITMQ-IP") + "/")
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func sendAkatsuki(data *pb.DataAkatsukiRequest) error {
+	conn, err := dialRabbitMQ()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	body, err := proto.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = ch.Publish(
+		"",     			  // exchange
+		"localizar_akatsuki", // routing key
+		false,  			  // mandatory
+		false,  			  // immediate
+		amqp091.Publishing{
+			ContentType: "application/protobuf",
+			Body:        body,
+		})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Sent: %+v\n", data.Id)
+	return nil
+}
+
+/**
+* Crea el canal "notificar_akatsuki" en RabbitMQ para enviar notificaciones a Akatsuki
+**/
+func initialize() {
+	conn, err := dialRabbitMQ()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer conn.Close()
+	
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"EquiposNinjaBrodcast", // name
+		"fanout",               // type
+		true,                   // durable
+		false,                  // auto-deleted
+		false,                  // internal
+		false,                  // no-wait
+		nil,                    // arguments
+	)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	q, _ := ch.QueueDeclare(
+		"notificar_akatsuki_Hokage", // name
+		false,  			  		 // durable
+		false,  			  		 // delete when unused
+		false,  			  		 // exclusive
+		false,  			  		 // no-wait
+		nil,    			  		 // arguments
+	)
+
+	log.Printf("Declared queue: %s", q.Name)
+
+	q, _ = ch.QueueDeclare(
+		"localizar_akatsuki", // name
+		false,  			  		 // durable
+		false,  			  		 // delete when unused
+		false,  			  		 // exclusive
+		false,  			  		 // no-wait
+		nil,    			  		 // arguments
+	)
+
+	log.Printf("Declared queue: %s", q.Name)
+}
+
+func notificarAkatsuki(data *pb.DataAkatsukiRequest) error {
+	conn, err := dialRabbitMQ()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	body, err := proto.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = ch.Publish(
+		"EquiposNinjaBrodcast",     	   // exchange
+		"", 							   // routing key
+		false,  			               // mandatory
+		false,  			               // immediate
+		amqp091.Publishing{
+			ContentType: "application/protobuf",
+			Body:        body,
+		})
+	if err != nil {
+		return err
+	}
+
+	err = ch.Publish(
+		"",     			  		 // exchange
+		"notificar_akatsuki_Hokage", // routing key
+		false,  			         // mandatory
+		false,  			         // immediate
+		amqp091.Publishing{
+			ContentType: "application/protobuf",
+			Body:        body,
+		})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Sent: %+v\n", data.Id)
+	return nil
+}
+
+/**
+* Recibe Akattsuki desde RabbitMQ desde el canal "localizar_akatsuki"
+* Origen: Akatsuki 
+**/
+func reciveLocalizarAkatsuki() {
+	conn, err := dialRabbitMQ()
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	msgs, err := ch.Consume(
+		"localizar_akatsuki", // queue
+		"",     			  // consumer
+		true,   			  // auto-ack
+		false,  			  // exclusive
+		false,  			  // no-local
+		false,  			  // no-wait
+		nil,    			  // args
+	)
+	if err != nil {
+		log.Fatalf("Failed to register a consumer: %v", err)
+	}
+
+    for d := range msgs {
+        var data pb.DataAkatsukiRequest
+
+        err := proto.Unmarshal(d.Body, &data)
+        if err != nil {
+            log.Println("decode error:", err)
+            continue
+        }
+
+		// Work Here
+        log.Printf("Received: %+v\n", data.Id)
+		notificarAkatsuki(&data)
+    }
+}
+
 
 func main() {
 	name := os.Getenv("NAME")
 	port := os.Getenv("PORT")
-
 	log.Printf("%s is running on port %s", name, port)
+
+	forever := make(chan bool)
+
+	initialize()
+
+	// Tarea de Recivir mensajes de RabbitMQ desde Akatsuki
+	go reciveLocalizarAkatsuki()
+
+	log.Printf("Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
--- Anbu/requirements.md
+++ Anbu/requirements.md
@@ -1,4 +1,23 @@
 # Anbu
+Unidad de iteligencia, monitorea la activdad de *Akatsuki*, detecta la presencia de enemigos y reporta al *Hokage*. Actúa como un sistema de vigilancia que recopila información sobre los enemigos y genera eventos quie alimentan el resto del sistema.
+
+## Tareas
+### Localizar Akatsuki
+Recibe información sobre los miembros activos de *Akatsuki* desde la entidad *Akatsulki*. Es decir, almacena un registro actualizado de los enemigos existentes en el sistema.
+
+- De manera **ASYNC**, *Akatsuki* envía la lista de enemigos disponibles al sistema de inteligencia *Anbu*.
+- Un akatsuki posee:
+    - Id    : Int
+    - Nombre: String
+    - Ataque: Int
+    - Vida:   Int
+    - Estado: Enum Estado (Localizado, En Combate, Capturado)
+
+### Entregar Lista de Akatsuki a Hokage
+Publica reportes de iteligencia hacia el *Hokage*, informando sobre enemigos detectados, su estado o cualquier cambio relevante en la información disponible.
+
+### Entregar Estado de Akatsuki que entren en Combate
+Cuando un miembro de *Akatsuki* entra en combate con un *Equipo ninja* y al *Hokage*. Este evento es publicado para infromar al sistema que el enemigo ya se encuentra ocupado y evitar que otros equipos intenten enfrentarlo simultáneamente.
 
 ## RabbitMQ
 La maquina de Anbu contendrá el servicio de RabbitMQ para todo el sistema.
